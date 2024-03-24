f6472f9ceda07d9b06ea530a3f6304724e8dacc8
Lingesh M <108582724+Lingeshmv@users.noreply.github.com>
2024-03-24 13:05:37 +0530

    Add files via upload

---
+++ evalution_test.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, LabelEncoder
+from sklearn.preprocessing import RobustScaler
+from sklearn.metrics import confusion_matrix, accuracy_score,classification_report
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from joblib import dump, load
+import pickle
+
+def evaluvation_metrics(x_test_path,y_test_path,model):
+
+    #Read data
+    X=pd.read_csv(x_test_path)
+    y_target=pd.read_csv(y_test_path)
+    #seperate numerical and categorical columns
+    numerical_columns=X.select_dtypes(exclude='object')
+    
+                       
+    # numerical-- Scalling
+    model_scaling=load('Model\standard_scaler.pkl')
+    scaled_data=model_scaling.transform(numerical_columns)
+    numerical_scaled_data=pd.DataFrame(scaled_data,columns=numerical_columns.columns)
+
+    Outcome = numerical_scaled_data
+
+
+    # model testing
+    log_reg=load(model)
+    y_pred=pd.DataFrame(log_reg.predict(Outcome))
+    test_score=accuracy_score(y_target, y_pred)*100
+    
+    
+    return y_pred, test_score
